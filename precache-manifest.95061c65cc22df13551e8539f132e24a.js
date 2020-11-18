3a4f5d038c6f710cca45d3ee7daced2b69ca1964
Tanay Sharda 11 <tanaysharda11@MacBook-Pro.local>
2020-11-19 01:34:51 +0530

    Updates

---
+++ precache-manifest.95061c65cc22df13551e8539f132e24a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5ed0aedb08c6b384a895e85404f7bf8",
+    "url": "/Diamonds/index.html"
+  },
+  {
+    "revision": "578727a082cf45313ea9",
+    "url": "/Diamonds/static/css/main.10b9de39.chunk.css"
+  },
+  {
+    "revision": "8954900975c85436d419",
+    "url": "/Diamonds/static/js/2.465310e4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Diamonds/static/js/2.465310e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "578727a082cf45313ea9",
+    "url": "/Diamonds/static/js/main.575b950d.chunk.js"
+  },
+  {
+    "revision": "1d62e775cbd44bcd4d89",
+    "url": "/Diamonds/static/js/runtime-main.f7c14b29.js"
+  },
+  {
+    "revision": "3e494450943a5ca2614b5d1a18650f01",
+    "url": "/Diamonds/static/media/40173EAC-4A28-4FA3-B2B5-6B6E9B1EB361.3e494450.JPG"
+  },
+  {
+    "revision": "1b5ccce997540438285d7272e88b60d8",
+    "url": "/Diamonds/static/media/IMG_0486.1b5ccce9.JPG"
+  },
+  {
+    "revision": "61aacd489f90b017eaf56d39454ba4d5",
+    "url": "/Diamonds/static/media/diamond.61aacd48.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Diamonds/static/media/logo.5d5d9eef.svg"
+  }
+]);
